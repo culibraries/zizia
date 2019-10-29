2b8fdbebab99de2eab962c8517d18f3dad167c32
Jamie Little <jamie@jamielittle.org>
2019-10-29 13:24:31 -0500

    Initial sortable tables
    This adds server-side sorting for the
    columns on the `zizia_csv_import_details` table.
    
    Sorting the additional headers will require more
    complex queries or a different solution.
    
    Connected to curationexperts/in-house#420

--- app/helpers/zizia/application_helper.rb
+++ app/helpers/zizia/application_helper.rb
@@ -1,6 +1,13 @@
 # frozen_string_literal: true
 module Zizia
   module ApplicationHelper
+    def sortable(column, title = nil)
+      title ||= column.titleize
+      css_class = column == sort_column ? "current #{sort_direction}" : nil
+      direction = column == sort_column && sort_direction == 'asc' ? 'desc' : 'asc'
+      link_to title, { sort: column, direction: direction }, class: css_class
+    end
+
     def collections_for_select
       ActiveFedora::SolrService.query('has_model_ssim:Collection').map do |c|
         [c['title_tesim'][0], c['id']]
