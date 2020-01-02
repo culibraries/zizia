aa924f9aedfae396e4eb25684cc570b4a9f863e5
Mark Stacy <mbstacy@gmail.com>
2020-01-02 12:40:33 -0700

    changed the default collecion return to 100

--- app/helpers/zizia/application_helper.rb
+++ app/helpers/zizia/application_helper.rb
@@ -2,7 +2,7 @@
 module Zizia
   module ApplicationHelper
     def collections_for_select
-      ActiveFedora::SolrService.query('has_model_ssim:Collection').map do |c|
+      ActiveFedora::SolrService.query('has_model_ssim:Collection',:rows => 100).map do |c|
         [c['title_tesim'][0], c['id']]
       end
     end
